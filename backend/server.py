e889112ff815bf11d94b18a4ded46498cb909a8e
Munira <144519028+MuniraGulova@users.noreply.github.com>
2025-10-05 23:57:04 +0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -93,6 +93,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
